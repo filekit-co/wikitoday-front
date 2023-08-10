bfbd8e5b65808b905b569685d9c7dec24ac32f0c
seilylook <seilylook@naver.com>
2023-08-10 22:36:14 +0900

    fix: error page todo: pagination

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,22 +3,31 @@
 </script>
 
 <main>
-  <div
-    class="h-screen w-full flex flex-col justify-center items-center bg-base-900"
-  >
-    <h1 class="text-9xl font-extrabol text-neutral-700 tracking-widest mb-8">
-      {$page.status}
-    </h1>
-    <div class="bg-accent px-2 text-4xl rounded-full mb-8">
-      {$page.error?.message || "Filekit.co"}
+  <div class="h-screen w-full flex justify-center items-center bg-base-900">
+    <div class="w-[500px] h-[500px]">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img
+        class="w-full h-full object-cover"
+        src="/error_image.png"
+        alt="error image"
+      />
     </div>
 
-    <div class="pt-5 join join-vertical md:join-horizontal">
-      <a href={`/`} class="join-item btn btn-outline text-6xl"> Go Home </a>
-    </div>
+    <div class="flex-col">
+      <h1 class="text-9xl font-extrabol text-neutral-700 tracking-widest mb-8">
+        {$page.status}
+      </h1>
+      <div class="bg-accent px-2 text-4xl rounded-full mb-8">
+        {$page.error?.message || "Filekit.co"}
+      </div>
 
-    <div class="pt-5 join join-vertical md:join-horizontal">
-      <a href={`/`} class="join-item btn btn-outline text-6xl"> Contact Us </a>
+      <div class="pt-5 join join-vertical md:join-horizontal">
+        <a href={`/`} class="join-item btn btn-outline text-6xl"> Go Home </a>
+      </div>
+
+      <div class="pt-5 join join-vertical md:join-horizontal">
+        <a href="/" class="join-item btn btn-outline text-6xl"> Contact Us </a>
+      </div>
     </div>
   </div>
 </main>
