cffede98c1551474bc4d421e42fb9d1d9635f12b
minkj1992 <minkj1992@gmail.com>
2023-08-11 21:14:34 +0900

    feat: slice page

---
+++ src/lib/consts.ts
@@ -0,0 +1,6 @@
+export const NUM_MAIN_ARTICLES = 10
+export const NUM_OPINIONS_ARTICLES = 35
+export const NUM_CARD_ARTICLES = 4
+
+
+export const getMaxArticles = () => Math.max(NUM_MAIN_ARTICLES, NUM_OPINIONS_ARTICLES, NUM_CARD_ARTICLES);
