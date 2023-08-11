73e7415fe34fdedf035bbd2f1e44265c69877b0e
minkj1992 <minkj1992@gmail.com>
2023-08-11 22:57:09 +0900

    update sitemap.xml

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,45 +1,47 @@
 
 import { generateSitemapRoutes } from "$lib/articles";
+import { LanguagePages } from "$lib/datas";
 
 export const prerender = true
 
 
-const languages = ['EN-US', 'KO', 'ZH', 'PT-BR', 'JA']; // 지원되는 언어 목록
+const languages = LanguagePages.map(page => page.value);
 
 async function getAllUrls() {
   const routesPromises = languages.map(generateSitemapRoutes);
   const allRoutes = await Promise.all(routesPromises);
-  const urls: string[] = allRoutes.flat();
-  return urls
+  const urls: { url: string; date: string }[] = allRoutes.flat();
+  return urls;
 }
 
-
 export async function GET() {
-  const urls: string[] = await getAllUrls()
+  const urls: { url: string; date: string }[] = await getAllUrls();
 
-  const headers = { 'Content-Type': 'application/xml' }
-	let sitemap = `
-	  <?xml version="1.0" encoding="UTF-8" ?>
-	  <urlset
-	    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-	    xmlns:xhtml="https://www.w3.org/1999/xhtml"
-	    xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-	    xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-	    xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-	    xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-	  >
+  // 최신 날짜 순으로 정렬
+  urls.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+  const headers = { 'Content-Type': 'application/xml' };
+  let sitemap = `
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <urlset
+      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xhtml="https://www.w3.org/1999/xhtml"
+      xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+      xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+      xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+    >
     ${urls
-      .map((url) => {
+      .map((urlObj) => {
         return `
           <url>
-            <loc>${url}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
+            <loc>${urlObj.url}</loc>
+            <lastmod>${urlObj.date}</lastmod>
           </url>
-        `
+        `;
       })
       .join('')}
-    </urlset>`.trim()
+    </urlset>`.trim();
 
-
-	return new Response(sitemap, { headers })
+  return new Response(sitemap, { headers });
 }
