2323ceaf0117aa1bab05822d4ddd0448b658620d
minkj1992 <minkj1992@gmail.com>
2023-08-12 00:55:42 +0900

    chore: fix bug

--- src/routes/[lang]/section/[category]/+page.server.ts
+++ src/routes/[lang]/section/[category]/+page.server.ts
@@ -1,5 +1,5 @@
 import { error } from "@sveltejs/kit";
-import type { Article } from "schema-dts";
+import type { Article } from "$lib/types";
 
 export async function load({fetch, params}) {
     const lang = params.lang;
--- src/routes/[lang]/section/[category]/+page.svelte
+++ src/routes/[lang]/section/[category]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import CategoryArticlePage from "@components/pages/CategoryArticlePage.svelte";
   import MainArticles from "@components/molecules/MainArticles.svelte";
-  import TrendNews from "@components/molecules/TrendNews.svelte";
 
   export let data;
   const articles = data.articles;
@@ -11,5 +10,4 @@
 
 <div class="lg:flex">
   <MainArticles {articles} />
-  <TrendNews {articles} />
 </div>
