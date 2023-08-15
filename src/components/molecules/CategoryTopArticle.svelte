0c884a5bcb1f6e72a8c97806249697a1d44e3e4d
seilylook <seilylook@naver.com>
2023-08-15 19:05:18 +0900

    design: category main page style change

--- src/components/molecules/CategoryTopArticle.svelte
+++ src/components/molecules/CategoryTopArticle.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import type { Article } from "$lib/types";
+  import { handleClick, formatDate } from "$lib/utils";
+
   export let articles: Article[];
-  import { handleClick } from "$lib/utils";
 </script>
 
-<div class=" shadow-xl rounded-3xl">
+<div class="shadow-xl rounded-3xl">
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <div
     on:click={() => handleClick(articles[0].slug)}
-    class="rounded-xl lg:flex py-4 px-2 items-center justify-center cursor-pointer duration-300 transform hover:scale-105 hover:shadow-2xl"
+    class="rounded-xl lg:flex py-4 px-2 justify-center items-center cursor-pointer duration-300 transform hover:scale-105 hover:shadow-2xl"
   >
     <div class="md:w-full lg:w-1/2 h-[250px] md:h-[450px] mb-4 lg:mb-0 lg:mr-4">
       <img
@@ -18,11 +19,21 @@
       />
     </div>
 
-    <div class="w-full lg:w-1/2">
-      <h1 class="md:text-2xl lg:text-3xl font-bold mb-4 title">
+    <div class="w-full lg:w-1/2 flex flex-col p-4 space-y-4">
+      <h1
+        class="text-2xl md:text-3xl lg:text-4xl font-extrabold leading-tight tracking-tighter mb-2"
+      >
         {articles[0].title}
       </h1>
-      <p class=" md:text-lg lg:text-xl description">
+      <p
+        class="text-sm lg:text-base font-semibold text-gray-600 mb-2 uppercase tracking-wide"
+      >
+        {articles[0].category}
+      </p>
+      <p class="text-xs lg:text-sm text-gray-400 mb-2">
+        {formatDate(articles[0].date)}
+      </p>
+      <p class="text-base md:text-lg lg:text-xl text-gray-700 leading-relaxed">
         {articles[0].description}
       </p>
     </div>
