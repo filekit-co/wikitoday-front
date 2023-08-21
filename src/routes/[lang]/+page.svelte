de03bf2630f1e5feb7c33af4341c14ed1d4de773
minkj1992 <minkj1992@gmail.com>
2023-08-21 11:01:08 +0900

    ads: add ads

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -5,6 +5,33 @@
   export let data: PageData;
 </script>
 
+<svelte:head>
+  <script type="text/javascript">
+    atOptions = {
+      key: "0a89ae418c6ec789390c9fe13c885c49",
+      format: "iframe",
+      height: 60,
+      width: 468,
+      params: {},
+    };
+    document.write(
+      "<scr" +
+        'ipt type="text/javascript" src="http' +
+        (location.protocol === "https:" ? "s" : "") +
+        '://www.profitablecreativeformat.com/0a89ae418c6ec789390c9fe13c885c49/invoke.js"></scr' +
+        "ipt>"
+    );
+  </script>
+  <script
+    type="text/javascript"
+    src="//pl20385247.highcpmrevenuegate.com/d8/49/da/d849dab39acb34ad7ceef994d713fca7.js"
+  ></script>
+  <script
+    type="text/javascript"
+    src="//pl20385564.highcpmrevenuegate.com/da/32/6c/da326c736ee63eda9bfb19290fc9e80a.js"
+  ></script>
+</svelte:head>
+
 <MainPage
   mainArticles={data.mainArticles}
   opinionArticles={data.opinionArticles}
--- src/routes/[lang]/section/[category]/+page.svelte
+++ src/routes/[lang]/section/[category]/+page.svelte
@@ -5,4 +5,23 @@
   export let data: PageData;
 </script>
 
+<svelte:head>
+  <script type="text/javascript">
+    atOptions = {
+      key: "83d8b4aa7dfbdbef30f7c137079151d2",
+      format: "iframe",
+      height: 250,
+      width: 300,
+      params: {},
+    };
+    document.write(
+      "<scr" +
+        'ipt type="text/javascript" src="http' +
+        (location.protocol === "https:" ? "s" : "") +
+        '://www.profitablecreativeformat.com/83d8b4aa7dfbdbef30f7c137079151d2/invoke.js"></scr' +
+        "ipt>"
+    );
+  </script>
+</svelte:head>
+
 <CategoryPage {data} />
