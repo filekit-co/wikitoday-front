5f3974b2e4dd2300621d74fcc39b4a7ff87765bf
minkj1992 <minkj1992@gmail.com>
2023-08-21 11:06:51 +0900

    ads: add banner ads

--- src/routes/[lang]/news/[date]/[article]/+page.svelte
+++ src/routes/[lang]/news/[date]/[article]/+page.svelte
@@ -5,6 +5,15 @@
   export let data: PageData;
 </script>
 
+<svelte:head>
+  <script
+    async="async"
+    data-cfasync="false"
+    src="//pl20385598.highcpmrevenuegate.com/a2ea6968735f78cd8e133fab7c7aa50b/invoke.js"
+  ></script>
+  <div id="container-a2ea6968735f78cd8e133fab7c7aa50b" />
+</svelte:head>
+
 <div class="max-w-3xl mx-auto">
   <article class="container relative pt-10">
     {@html data.articleHtml}
